403e88811ee2327c6e48aa830232ed35c1a7c9bd
netmon39 <m.netipol@gmail.com>
2016-06-25 11:16:17 +0700

    Try playing around with the new camera feature.

--- app/src/main/java/net/zooth/ZooCamera.java
+++ app/src/main/java/net/zooth/ZooCamera.java
@@ -1,56 +1,194 @@
 package net.zooth;
 
+import android.content.ContentResolver;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.hardware.Camera;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.ContactsContract.CommonDataKinds.Im;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.text.method.Touch;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class ZooCamera extends Activity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
-    Button btnTakePhoto;
-    ImageView imgTakenPhoto;
+public class ZooCamera extends Activity implements View.OnClickListener{
+
+    private Button btnTakePhoto, cam, sav;
+    TouchImageView imgTakenPhoto, f;
     private static final int CAM_REQUEST = 1313;
+
+    private static final int TAKE_PICTURE = 1;
+    private Uri imageUri;
+    public boolean saveboo=false;
+    public Bitmap bittt=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_zoo_camera);
+        //setContentView(R.layout.activity_zoo_camera);
+        //btnTakePhoto = (Button) findViewById(R.id.button);//
+        //imgTakenPhoto = (ImageView) findViewById(R.id.imageview);
+        //btnTakePhoto.setOnClickListener(new btnTakePhotoClicker());*/
 
-        btnTakePhoto = (Button) findViewById(R.id.button);
-        imgTakenPhoto = (ImageView) findViewById(R.id.imageview);
+        /*Intent cameraintent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraintent, CAM_REQUEST);*/
 
-        btnTakePhoto.setOnClickListener(new btnTakePhotoClicker());
+        //File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");//HTC/Internal Storage/Pic..jpg
+
+        //SAVING MECHANISM to save move to call up camera
+        /*String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/ZooTH_Photos/My Photos");
+        myDir.mkdirs();
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-"+ n +".jpg";
+        File photo = new File (myDir, fname);//***
+        if (photo.exists ()) photo.delete ();*/
+
+        callUpCamera();
     }
 
+    public void callUpCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStorageDirectory() + "/ZooTH/temp/.nomedia",  "temp.jpg");//HTC/Internal Storage/Pic..jpg
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,//SAVE***
+                Uri.fromFile(photo));
+        imageUri = Uri.fromFile(photo);
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
 
+    public Bitmap overlayFrame(Bitmap bmp1, Bitmap bmp2) {
+        Bitmap bmOverlay = Bitmap.createBitmap(bmp1.getWidth(), bmp1.getHeight(), bmp1.getConfig());
+        Canvas canvas = new Canvas(bmOverlay);
+
+        canvas.drawBitmap(bmp1, new Matrix(), null);
+        Rect fittoframe = new Rect(0,0,bmp1.getWidth(),bmp1.getHeight());
+        canvas.drawBitmap(bmp2, null, fittoframe, null);//Stretch frame to completely cover bmp1
+        return bmOverlay;
+    }
+
+    public void saveBit(Bitmap bitmp){
+        {//IF save button is clicked, save the bitmp
+            File myDir = new File(Environment.getExternalStorageDirectory() + "/ZooTH/ZooTH Photos");
+            myDir.mkdirs();
+            Random generator = new Random();
+            int n = 10000;
+            n = generator.nextInt(n);
+            String fname = "Image-" + n + ".jpg";
+            File fil = new File(myDir, fname);
+            //getContentResolver().notifyChange(Uri.fromFile(fil), null);
+            if (fil.exists()) fil.delete();
+            FileOutputStream out = null;
+            try {
+                out = new FileOutputStream(fil);
+                bitmp.compress(Bitmap.CompressFormat.JPEG, 90, out); // bmp is your Bitmap instance
+                // PNG is a lossless format, the compression factor (100) is ignored
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            MediaScannerConnection.scanFile(this, new String[]{fil.getPath()}, new String[]{"image/jpeg"}, null);//shows up in gallery
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // TODO Auto-generated method stub
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == CAM_REQUEST)
+        super.onActivityResult(requestCode, resultCode, data);//data is taken photo
+        /*if(requestCode == CAM_REQUEST && resultCode == RESULT_OK)
         {
+            setContentView(R.layout.activity_zoo_camera);
             Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            imgTakenPhoto = (ImageView) findViewById(R.id.takenphotopreview);
             imgTakenPhoto.setImageBitmap(thumbnail);
+
+
+        } else {
+            Toast.makeText(getApplicationContext(), "Photo not taken!", Toast.LENGTH_LONG).show();
+        }}*/
+
+        switch (requestCode) {
+            case TAKE_PICTURE:
+                if (resultCode == RESULT_OK) {
+                    setContentView(R.layout.activity_zoo_camera);
+                    Uri selectedImage = imageUri;
+                    getContentResolver().notifyChange(selectedImage, null);
+
+                    imgTakenPhoto = (TouchImageView) findViewById(R.id.takenphotopreview);
+                    imgTakenPhoto.setMaxZoom(5);
+
+                    ContentResolver cr = getContentResolver();
+                    Bitmap bitmap;
+                    try {
+                        bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, selectedImage);
+
+                        Drawable myDrawable = getResources().getDrawable(R.drawable.zooframe);//Frame name here
+                        Bitmap frame = ((BitmapDrawable) myDrawable).getBitmap();
+
+                        bittt = overlayFrame(bitmap, frame);//Combine taken photo with preset frame
+
+                        sav = (Button) findViewById(R.id.save);
+                        sav.setOnClickListener(this);
+                        cam = (Button) findViewById(R.id.camera);
+                        cam.setOnClickListener(this);
+
+                        imgTakenPhoto.setImageBitmap(bittt);//Show temp.jpg taken photo here!
+                        //Toast.makeText(this, selectedImage.toString(),Toast.LENGTH_LONG).show();
+
+                    } catch (Exception e) {
+                        Toast.makeText(this, "Failed to load.", Toast.LENGTH_SHORT).show();
+                        Log.e("Camera", e.toString());
+                    }
+                    break;
+
+                }
+            default:
+                Toast.makeText(getApplicationContext(), "No photo taken! Press Back.", Toast.LENGTH_SHORT).show();
+                break;
         }
+
 
     }
 
-    class btnTakePhotoClicker implements Button.OnClickListener
-    {
-
-        @Override
-        public void onClick(View v) {
-            // TODO Auto-generated method stub
-            Intent cameraintent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(cameraintent, CAM_REQUEST);
-        }
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.save:
+                saveBit(bittt);
+                Toast.makeText(this, "Saved.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.camera:
+                Intent intent = new Intent(this, ZooCamera.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                break;
     }
-}
+}}
